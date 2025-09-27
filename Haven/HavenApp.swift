02d2279e5f523adfe14cafd78c47b3cc98664081
samvitb <samvit.bhattacharya@gmail.com>
2025-09-27 00:53:45 -0700

    Initial Commit

---
+++ Haven/HavenApp.swift
@@ -0,0 +1,17 @@
+//
+//  HavenApp.swift
+//  Haven
+//
+//  Created by samvit bhattacharya on 9/27/25.
+//
+
+import SwiftUI
+
+@main
+struct HavenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HavenTests/HavenTests.swift
@@ -0,0 +1,17 @@
+//
+//  HavenTests.swift
+//  HavenTests
+//
+//  Created by samvit bhattacharya on 9/27/25.
+//
+
+import Testing
+@testable import Haven
+
+struct HavenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
